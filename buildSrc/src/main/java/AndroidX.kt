ab4a4b3ae48c59eec8cb846469842ebe04c37ee3
Paula Gaitan <paulagaiitan@gmail.com>
2023-11-24 12:29:13 -0500

    feature(buildSrc, app ): Se agrega directorio de build en el proyecto, para el manejo de dependencias.

---
+++ buildSrc/src/main/java/AndroidX.kt
@@ -0,0 +1,19 @@
+object AndroidX {
+    private const val coreKtxVersion = "1.9.0"
+    private const val lifecycleRuntimeVersion = "2.6.1"
+    private const val appCompatVersion = "1.4.2"
+    private const val coreSplashsVersion = "1.0.0"
+    private const val workDescargaVersion = "2.7.1"
+    private const val constraintLayoutVersion = "2.1.4"
+    private const val biometriaVersion = "1.1.0"
+    private const val lifecycleRuntimeKtxVersion = "2.5.1"
+
+    const val biometria = "androidx.biometric:biometric:$biometriaVersion"
+    const val coreKtx = "androidx.core:core-ktx:$coreKtxVersion"
+    const val lifecycleKtx = "androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleRuntimeKtxVersion"
+    const val lifecycleRuntime = "androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleRuntimeVersion"
+    const val appCompat = "androidx.appcompat:appcompat:$appCompatVersion"
+    const val coreSplash = "androidx.core:core-splashscreen:$coreSplashsVersion"
+    const val workDescarga = "androidx.work:work-runtime-ktx:$workDescargaVersion"
+    const val constraintLayout = "androidx.constraintlayout:constraintlayout:$constraintLayoutVersion"
+}
---
+++ buildSrc/src/main/java/ConfiguracionProyecto.kt
@@ -0,0 +1,8 @@
+object ConfiguracionProyecto {
+    const val applicationId = "com.gaitan.dev.mobileboldandroid"
+    const val compileSdk = 33
+    const val minSdk = 21
+    const val targetSdk = 33
+    const val versionCode = 1
+    const val versionName = "1.0"
+}
