8a25aec6baa766bee5906ff70065e68480814a01
Paula Gaitan <paulagaiitan@gmail.com>
2023-11-25 13:11:43 -0500

    feat(app): Implementacion de api key segura

--- clima/clima_presentacion/build.gradle.kts
+++ clima/clima_presentacion/build.gradle.kts
@@ -2,9 +2,10 @@ apply {
     from("$rootDir/composeui-base.gradle")
 }
 
+
 dependencies {
     "implementation"(Coil.coilCompose)
-
+    "implementation"(Compose.constraintLayoutCompose)
     "implementation"(project(Modulos.baseUi))
     "implementation"(project(Modulos.climaDatos))
     "implementation"(project(Modulos.climaDominio))
